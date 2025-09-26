b4d2ae7da9cbd2affaf2d2c455f0a20af7c42eaa
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-09-26 14:28:26 -0600

    Update calculate_fid.py

--- src/quality_metrics/calculate_fid.py
+++ src/quality_metrics/calculate_fid.py
@@ -1,1 +1,11 @@
+from scipy.linalg import sqrtm
+import numpy as np
+
+def calculate_fid(mu1, sigma1, mu2, sigma2):
+    diff = mu1 - mu2
+    covmean = sqrtm(sigma1.dot(sigma2))
+    if np.iscomplexobj(covmean):
+        covmean = covmean.real
+    fid = diff.dot(diff) + np.trace(sigma1 + sigma2 - 2 * covmean)
+    return fid
 
