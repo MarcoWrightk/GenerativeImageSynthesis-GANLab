e445798b5777e86d58d18a36b0f9443ddbaa15bd
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-09-26 20:59:39 -0600

    Update faces_dataset.py

--- datasets/faces/faces_dataset.py
+++ datasets/faces/faces_dataset.py
@@ -1,1 +1,21 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class FacesDataset(Dataset):
+    def __init__(self, folder, image_size=64):
+        self.files = [os.path.join(folder, f) for f in os.listdir(folder)]
+        self.transform = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])
+        ])
+
+    def __getitem__(self, index):
+        img = Image.open(self.files[index]).convert("RGB")
+        return self.transform(img)
+
+    def __len__(self):
+        return len(self.files)
 
