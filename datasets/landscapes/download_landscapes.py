c738e6c821169924744340e16bc63932b745425d
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-09-26 21:19:16 -0600

    Update download_landscapes.py

--- datasets/landscapes/download_landscapes.py
+++ datasets/landscapes/download_landscapes.py
@@ -1,1 +1,10 @@
+import os
+import gdown
+
+def download_places365(output_dir="landscapes/raw"):
+    os.makedirs(output_dir, exist_ok=True)
+    url = "https://drive.google.com/uc?id=my_PLACES365_ID" 
+    output_path = os.path.join(output_dir, "places365.zip")
+    gdown.download(url, output_path, quiet=False)
+    print(f"Dowloaded in: {output_path}")
 
